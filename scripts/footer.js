8d18fb05d6a12dfbbc6b9068a74dff36f16bf842
Priyanka4251 <97400383+Priyanka4251@users.noreply.github.com>
2022-07-23 20:07:03 +0530

    Add files via upload

---
+++ scripts/footer.js
@@ -0,0 +1,71 @@
+
+
+let footer=()=>{
+    return `
+    <div id="P-footer">
+    <div id="P-footer_box1">
+        <p style="font-weight: bold; margin-bottom: 10px;">Get Expert Beauty Insight & Offers</p>
+        <p><input type="email" id="P-email" placeholder="EMAIL ADDRESS" > <i style="font-size: 20px; margin: 10px 0 0 -20px;" class="fa-solid fa-arrow-right"></i> </p>
+        <div class="privacy">
+        <p>By continuing, you agree to Bluemercury's </p>
+        <p><a href="#" id="Privacy_policy">Privacy Practices.</a></p>
+    </div>
+        <P id="P-social">
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-pinterest-p"></i>
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-youtube"></i>
+        </P>
+        <p>Copyright 2022 bluemercury. All Rights Reserved.</p>
+    </div>
+    <div id="P-footer_box2">
+        <h2 id="P-h2">BLUEMERCURY</h2>
+        <p>Our Company</p>
+        <p>Store Locator</p>
+        <p>In-Store & Online Events</p>
+        <p>Spa Menu</p>
+        <p>Careers</p>
+        <p>Vendor Submissions</p>
+        <p>Diversity & Inclusion</p>
+    </div>
+    <div id="P-footer_box3">
+        <h2 id="P-h2">MY ACCOUNT</h2>
+        <p>My Account</p>
+        <p>BlueRewards</p>
+        <p>Gift Card Balance Checker</p>
+    </div>
+    <div id="P-footer_box4">
+        <h2 id="P-h2">CUSTOMER SERVICE</h2>
+        <p>Contact Us</p>
+        <p>Help & FAQ</p>
+        <p>Shipping, Returns & Exchanges</p>
+        <p>Gift Cards</p>
+        <p>Offers & Promotions</p>
+        <p>Klarna</p>
+    </div>
+    <div id="P-footer_box5">
+        <h2 id="P-h2">RESOURCES</h2>
+        <p>Terms & Conditions</p>
+        <p>Privacy Notice</p>
+        <p>Do Not Sell My Personal Information</p>
+        <p>CA Privacy Rights</p>
+        <p>Accessibility</p>
+        <p>Affiliate Program</p>
+        <p>COVID-19 Updates</p>
+    </div>
+</div>
+
+<!-- ------------------------footer2--------------------------- -->
+
+<div id="P-footer2Box1">
+    <p>We use cookies on our website. By clicking "Accept", you consent to the use of All the cookies. You may change your preference by visiting Cookie Settings.</p>
+    <p><a href="#">Cookie Settings</a>
+    <button id="btn1_P"; onclick="Accept_Decline()">Accept</button>
+    <button id="btn2_P"; onclick="Accept_Decline()">Decline</button>
+    </p>
+</div>
+    `
+}
+
+export default footer;
